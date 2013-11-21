78bb457538f926d8bb1b37ba96fcfe9163ab4e7c
Prince781 <princetonf@optonline.net>
2013-11-21 09:05:16 -0500

    Adding Spotify auto-installation script.

---
+++ spotify_install/spotify_install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+if [ $UID -eq 0 ]; then
+	echo "It is not recommended to run this as the root user."
+	exit 1
+fi
+
+if [ -e /bin/dnf ]; then
+	PROG=/bin/dnf
+else
+	PROG=/bin/yum
+fi
+
+echo "Installing essential build tools..."
+
+DEPS="wget rpmdevtools rpm-build yum-utils"
+
+$PROG -y install $DEPS
+
+rpmdev-setuptree
+
+cd $(rpm --eval %_sourcedir)
+
+wget "http://leamas.fedorapeople.org/spotify/latest/spotify-client.spec"
+
+spectool -g "spotify-client.spec" && sudo yum-builddep "spotify-client.spec"
+
+env QA_RPATHS=$((0x10|0x08)) rpmbuild -bb "spotify-client.spec"
+
+echo "Install the Spotify package? Enter \"y\" to install, or nothing otherwise."
+
+read toInstall
+
+if [ $toInstall == "y" ]; then
+	$PROG -y install ~/rpmbuild/RPMS/`uname -m`/spotify-client*.rpm
+fi
+
