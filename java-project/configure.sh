6d43dd92ff54ba02693f390989ded213fb3c3340
Prince781 <princetonf@optonline.net>
2014-02-14 19:52:15 -0500

    Support for autogenerated manifest files.

--- java-project/configure.sh
+++ java-project/configure.sh
@@ -6,9 +6,13 @@ function display_help() {
 \tDisplays this output.
 --initial, -i
 \tSets up the initial directory hierarchy.
---manifest, -m
-\tSets up an initial manifest file.\n"
+--manifest, -m [CLASS NAME]
+\tSets up an initial manifest file, with optional class name.\n"
 }
+
+mf_file="manifest.mf"
+manifest_level=0
+mf_classname=""
 
 for arg in "$@"; do
 	case $arg in
@@ -18,22 +22,35 @@ for arg in "$@"; do
 			if [ ! -e $fld ]; then mkdir $fld; fi
 		done
 		# populate src
-		if [ ! -e manifest.mf ]; then touch manifest.mf; fi
+		if [ ! -e $mf_file ]; then touch $mf_file; fi
 		;;
 	"--manifest" | "-m") # set up manifest file
-		if [ -e manifest.mf ]; then continue; fi
-		echo "Manifest-Version: 1.0" > manifest.mf
-		printf "Name: %s\n" `basename $((pwd))` > manifest.mf
-		printf "Class-Path: %s" `find lib -type f -name '*.java' | sed ':a;N;$$!ba;s/\n/\n /g'` > manifest.mf
+		manifest_level=$(($manifest_level + 1))
 		;;
 	"--help" | "-h") # default
 		display_help
 		;;
 	*)
-		printf "Unknown option. Pass --help or -h for more information.\n"
-		exit 1
+		if [ $manifest_level == 1 ]; then
+			mf_classname=$arg
+			manifest_level=$(($manifest_level + 1))
+		else
+			printf "Unknown option. Pass --help or -h for more information.\n"
+			exit 1
+		fi
 		;;
 	esac
 done
+
+if [ $manifest_level -gt 0 ]; then # set up manifest
+	classes=". `find lib -type f -name '*.jar' | sed ':a;N;$!ba;s/\n/ /g'`"
+	echo "Manifest-Version: 1.0" > $mf_file # erase from start
+	printf "Name: %s\n" "`basename "$(pwd)"`" >> $mf_file
+	if [ $manifest_level -gt 1 ]; then
+		printf "Main-Class: %s\n" $mf_classname >> $mf_file
+	fi
+	printf "Class-Path:" >> $mf_file
+	printf " %s\n" "`echo $classes | fold -s -w 72 | sed ':a;N;$!ba;s/\n/\n /g'`" >> $mf_file
+fi
 
 if [ $# -eq 0 ]; then display_help; fi
