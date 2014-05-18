b721a53209f8f9cd6ad489eec21032f645fb8114
Prince781 <princetonf@optonline.net>
2014-05-18 04:25:43 -0400

    Update for factorial.

--- mathtools/mathf.c
+++ mathtools/mathf.c
@@ -1,10 +1,23 @@
 #include "mathf.h"
+#include <math.h>
 
 float sqrtf(float n) {
-	unsigned int v1 = (unsigned int) n,
-		v2 = 0;
-	// shitty implementation (TODO: remove loop)
-	while (v2 - v1 > SQRTF_LIMIT)
-		// use newton method
+	float s1 = 1, s2 = 2;
+	int i;
 	
+	for (i=0; s2 - s1 > SQRTF_THRESHOLD; i++) {
+		if (i%2) s2 = s1 - (s1*s1 - n)/(2*s1);
+		else s1 = s2 - (s2*s2 - n)/(2*s2);
+	}
+	
+	return i%2 ? s1 : s2;
+}
+
+unsigned long int factorial(int n) {
+	float sum = 0;
+	int i;
+	for (i=2; i<=n; i++)
+		sum += log(i);
+	
+	return (unsigned long int)(exp(sum)+1);
 }
